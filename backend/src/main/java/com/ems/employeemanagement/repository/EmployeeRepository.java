bdea2b373efc700ea4ae2ae04cc4b1f8f77e3a72
Anissha M <anissha1608@gmail.com>
2026-07-08 12:27:15 +0530

    Update EmployeeRepository.java
    Removed unnecessary comments from EmployeeRepository.

--- backend/src/main/java/com/ems/employeemanagement/repository/EmployeeRepository.java
+++ backend/src/main/java/com/ems/employeemanagement/repository/EmployeeRepository.java
@@ -4,11 +4,6 @@ import com.ems.employeemanagement.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repository for Employee entity.
- * Spring Data JPA automatically provides CRUD methods like
- * save(), findAll(), findById(), deleteById(), etc.
- */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
