eaf95ae69df80dccff3ce094d50b06447f27e2d8
Anissha M <anissha1608@gmail.com>
2026-07-08 12:27:37 +0530

    Update EmployeeManagementApplication.java
    Removed the Javadoc comment describing the entry point of the application.

--- backend/src/main/java/com/ems/employeemanagement/EmployeeManagementApplication.java
+++ backend/src/main/java/com/ems/employeemanagement/EmployeeManagementApplication.java
@@ -3,10 +3,7 @@ package com.ems.employeemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Entry point of the Employee Management System backend.
- * Run this class to start the Spring Boot application.
- */
+
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
